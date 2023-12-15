eca1d9406aa19dcdb07b505d85816cd0d61db397
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2023-12-16 00:00:41 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProcessFlow from "./components/process";
 import ProjectFilter from "./components/projects";
 import TeamMembers from "./components/team";
 import Slider from "./components/slider";
+import Footer from "./components/footer"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <ProjectFilter />
       <TeamMembers />
       <Slider />
+      <Footer />
     </>
   );
 }
